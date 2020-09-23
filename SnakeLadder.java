69f232d7ee1c82bbf15f1fdea195e40295c6e745
Jagriti <jagriti.agni@gmail.com>
2020-09-23 20:06:24 +0530

    [Jagriti]ADDED: Result of rolling dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,16 +2,14 @@ public class SnakeLadder {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game.");
-		
-		// defining game variables 
-		int SAFE_PLACE = 0;
-		int SNAKE_PLACE = 1;
-		int LADDER_PLACE = 2;
 
-		int dieRoll = 0;							// number came on rolling dice
 		//UC1 number of players = 1 and position = 0
 		int player1Position = 0;              // when position = 100 player wins
-
 		System.out.println("Player one position: " + player1Position);
+
+		// UC2 the outcome of dice
+		int dieRollOutcome = ((int) Math.floor(Math.random() * 10 ) % 6) +1;		// number came on rolling dice
+		System.out.println("The outcome on rolling the die: " + dieRollOutcome);
+
 	}
 }
