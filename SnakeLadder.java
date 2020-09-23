f1f62be0f5b02cbb46e87e6cd4cbcc6835eb22a6
Jagriti <jagriti.agni@gmail.com>
2020-09-23 20:40:28 +0530

    [Jagriti]ADDED: Player reaches the winning position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,28 +2,35 @@ public class SnakeLadder {
 	public static final int NO_PLAY = 0;			// no change in player position
 	public static final int SNAKE = 1;				// decrement player position
 	public static final int LADDER = 2;				// increment player position
+	public static final int WIN_POSITION = 100;				// players wins if final position is 100
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to snake and ladder game.");
 
 		//UC1 number of players = 1 and position = 0
-		int player1Position = 0;              // when position = 100 player wins
-		System.out.println("Player one position: " + player1Position);
+		int player1Position = 0;              // when initial position = 0
+		
+		//UC4 players wins if final position is WIN_POSITION
+		while (player1Position <= WIN_POSITION) {
+			//System.out.println("Player one position: " + player1Position);
+			
+			int dieRollOutcome = ((int) Math.floor(Math.random() * 10 ) % 6) +1;		// number came on rolling dice
+			//System.out.println("The outcome on rolling the die: " + dieRollOutcome);
 
-		// UC2 the outcome of dice
-		int dieRollOutcome = ((int) Math.floor(Math.random() * 10 ) % 6) +1;		// number came on rolling dice
-		System.out.println("The outcome on rolling the die: " + dieRollOutcome);
+			int playPositionOption = ((int) Math.floor(Math.random() * 10 ) % 3);	//type- NO_PLAY, SNAKE or LADDER
+			//System.out.println("Type of option: " + playPositionOption);
 
-		// UC3 change position based on type of option
-		int playPositionOption = ((int) Math.floor(Math.random() * 10 ) % 3);	//type- NO_PLAY, SNAKE or LADDER
-		System.out.println("Type of option: " + playPositionOption);
-
-		if(playPositionOption == LADDER) {
-			player1Position += dieRollOutcome;
-		} else if (playPositionOption == SNAKE) {
-			player1Position -= dieRollOutcome;
+			if(playPositionOption == LADDER) {
+				player1Position += dieRollOutcome;
+			} else if (playPositionOption == SNAKE) {
+				player1Position -= dieRollOutcome;
+			}
+			if (player1Position < 0) {
+				player1Position = 0;				//game restarts if position moves below 0		
+			}
 		}
+		
 
-		System.out.println("The final position of player 1: "+ player1Position );
+		System.out.println("Player wins!!! The final position of player 1: "+ player1Position);
 	}
 }
